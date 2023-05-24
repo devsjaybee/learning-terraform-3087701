7bec2f2ea681b609119f2b194958aefdebbcee62
Jaybeedev <ec2-user@ip-172-31-37-53.ap-southeast-1.compute.internal>
2023-05-24 02:17:06 +0000

    Create security group

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,15 @@
 output "instance_ami" {
+<<<<<<< HEAD
  value = aws_instance.blog.ami
 }
 
 output "instance_arn" {
  value = aws_instance.blog.arn
+=======
+  value = aws_instance.web.ami
+}
+
+output "instance_arn" {
+  value = aws_instance.web.arn
+>>>>>>> 8779a1b (Create security group)
 }
