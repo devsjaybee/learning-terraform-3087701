ac0d0be57d061ee61e447cd8ef7f0642098b609f
Jaybeedev <ec2-user@ip-172-31-37-53.ap-southeast-1.compute.internal>
2023-05-25 11:45:22 +0000

    Remove natgateway

--- main.tf
+++ main.tf
@@ -23,7 +23,7 @@ module "blog_vpc" {
   azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
-  enable_nat_gateway = true
+  
   
 
   tags = {
@@ -53,7 +53,7 @@ module "blog-alb" {
 
   vpc_id             = module.blog_vpc.vpc_id
   subnets            = module.blog_vpc.public_subnets
-  security_groups    = module.blog_sg.security_group_id
+  security_groups    = [module.blog_sg.security_group_id]
 
 
   target_groups = [
